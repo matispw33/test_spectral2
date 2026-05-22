8ec27eb21c4d86b1dc50433901f2ea8c4b0209e5
matif33 <matias.ferreras@smartbear.com>
2026-05-22 17:15:59 +0200

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxMzA2Nw/spectral.js?branch_slug=main&token=e1aca366-c52b-425f-8edb-61b605919a3e";
+export default { extends: ruleset };
